aa8d2aaa4676ab30c8730a017f60e05e85c04253
nostalgebraist <nostalgebraist@gmail.com>
2022-11-28 14:01:05 -0800

    warmup rate

--- ldm/modules/ema.py
+++ ldm/modules/ema.py
@@ -3,7 +3,7 @@ from torch import nn
 
 
 class LitEma(nn.Module):
-    def __init__(self, model, decay=0.9999, use_num_upates=True):
+    def __init__(self, model, decay=0.9999, use_num_upates=True, warmup_rate=2):
         super().__init__()
         if decay < 0.0 or decay > 1.0:
             raise ValueError('Decay must be between 0 and 1')
@@ -22,6 +22,8 @@ class LitEma(nn.Module):
 
         self.collected_params = []
 
+        self.warmup_rate = warmup_rate
+
     def reset_num_updates(self):
         del self.num_updates
         self.register_buffer('num_updates', torch.tensor(0, dtype=torch.int))
@@ -31,7 +33,7 @@ class LitEma(nn.Module):
 
         if self.num_updates >= 0:
             self.num_updates += 1
-            decay = min(self.decay, (1 + self.num_updates) / (10 + self.num_updates))
+            decay = min(self.decay, (1 + self.num_updates) / (self.warmup_rate + self.num_updates))
 
         one_minus_decay = 1.0 - decay
 
