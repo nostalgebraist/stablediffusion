d49deb414c0559ef9ab8cac54c53f58459e79687
nostalgebraist <nostalgebraist@gmail.com>
2022-12-04 13:03:39 -0800

    sync

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -1386,6 +1386,10 @@ class LatentUpscaleDiffusion(LatentDiffusion):
         with torch.cuda.amp.autocast(False):
             return super().decode_first_stage(z.float(), **kwargs)
 
+    def encode_first_stage(self, x, **kwargs):
+        with torch.cuda.amp.autocast(False):
+            return super().encode_first_stage(e.float(), **kwargs)
+
     @torch.no_grad()
     def get_input(self, batch, k, cond_key=None, bs=None, log_mode=False):
         if not log_mode:
